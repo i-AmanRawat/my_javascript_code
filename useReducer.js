f9377276983d1e98c215e960f38a47e5be28cfb5
Aman Rawat <94544552+i-AmanRawat@users.noreply.github.com>
2023-05-31 14:45:37 +0530

    Update useReducer.js
    added comments

--- useReducer.js
+++ useReducer.js
@@ -1,6 +1,9 @@
 import { useReducer } from 'react'
+//The useReducer() hook is a built-in hook in React that provides a way to manage complex state logic by using a reducer function. 
+//It is an alternative to the useState() hook when you need to manage state transitions that involve multiple actions or have more complex logic.
 
-function reducer(state, action){
+//The reducer function defines how the state is updated based on different actions. It receives the current state and an action as arguments and returns the new state.
+function reducer(state, action){  //reducer function
   switch(action.what){
     case 'increment':{
       return {
@@ -17,10 +20,13 @@ function reducer(state, action){
     }
   }
 }
+//The useReducer() hook is useful when you have complex state logic that involves multiple actions or requires derived state calculations. 
+//It provides a more organized way to handle state transitions and can be especially beneficial for larger-scale applications.
 
 export default function App(){
 //   const [state, dispatch] = useReducer(reducer, initArg, initFunc)
-  const [state, dispatch] = useReducer(reducer, {num: 0} )
+  //dispatch func, is essentially what we call inorder to update our state, with a action we want to perform on our state
+  const [state, dispatch] = useReducer(reducer, {num: 0} )    //takes two arguments: the reducer function and the initial state. generally we wrap intial state value inside a object
   return(
     <>
       <button onClick={()=>dispatch({what: 'decrement'})}>-</button>
