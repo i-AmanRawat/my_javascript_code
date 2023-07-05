3df1011e0c781dbf7b6d4726e7c1a332de3b7106
Aman Rawat <94544552+i-AmanRawat@users.noreply.github.com>
2023-07-05 10:59:32 +0530

    Update server.js
    delete method added and put method updated

--- express/server.js
+++ express/server.js
@@ -8,14 +8,14 @@ function middleware(req, res, next) {
 app.use(middleware);
 app.use(express.json());
 
-const ideas = {
-  1: {
+const ideas = [
+  {
     id: 1,
     idea_name: "i don't have any fucking idea!",
     author_name: "aman rawat",
     idea_description: "ab kya description du bhai",
   },
-};
+];
 
 /*
     Search all ideas
@@ -24,7 +24,7 @@ function allIdeas(req, res) {
   res.status(200).json(ideas);
 }
 
-app.get("/ideaApp/ideas", allIdeas);
+app.get("/ideaApp/ideas/all", allIdeas);
 
 /*
 create a new idea
@@ -33,20 +33,21 @@ function createIdea(req, res) {
   const idea = req.body;
   let id = Object.keys(ideas).length + 1;
   idea["id"] = id;
-  ideas[id] = idea;
-  res.status(201).json(ideas[id]);
+  ideas.push(idea);
+  res.status(201).json(ideas[id - 1]);
 }
 
-app.post("/ideaApp/ideas", createIdea);
+app.post("/ideaApp/ideas/newIdea", createIdea);
 
 /*
 update an existing idea
 */
 function updateIdea(req, res) {
   const ideaId = req.params.id;
-  if (ideas[ideaId]) {
-    ideas[ideaId] = req.body;
-    res.status(200).json(ideas[ideaId]);
+  const ideaIndex = ideaId - 1;
+  if (ideas[ideaIndex]) {
+    ideas[ideaIndex] = req.body;
+    res.status(200).json(ideas[ideaIndex]);
   } else {
     res.status(400).json({
       message: "invalid id, enter a valid id of idea to be updated",
@@ -57,8 +58,29 @@ function updateIdea(req, res) {
 app.put("/ideaApp/ideas/update/:id", updateIdea);
 
 /*
+delete an idea from ideas
+*/
+function deleteIdea(req, res) {
+  const deleteId = Number(req.params.id);
+  const deleteIndex = deleteId - 1;
+
+  if (ideas.length >= deleteId) {
+    ideas.splice(deleteIndex, 1);
+    res.status(200).json({
+      message: `successfully deleted idea with id: ${deleteId}`,
+    });
+  } else {
+    res.status(400).json({
+      message: `invalid id, no idea available with id: ${deleteId}`,
+    });
+  }
+}
+
+app.delete("/ideaApp/ideas/delete/:id", deleteIdea);
+/*
 listen to 8080 port 
 */
 app.listen(8080, () => {
   console.log("server is running at 8080");
 });
+
